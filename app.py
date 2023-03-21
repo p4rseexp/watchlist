8bd28101bec49344dc48f49665d6e8433066f06d
p4rseexp <p4rseexp@gmail.com>
2023-03-21 16:23:59 +0800

    Add minimal home page

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import url_for
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def hello():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User: {escape(name)}'
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('user_page', name='bob'))
+    print(url_for('user_page', name='pacman'))
+    print(url_for('test_url_for'))
+    print(url_for('test_url_for', num=2))
+
+    return 'Test Page'
