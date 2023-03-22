bd9fe07569ccd5851abb794ab4b87c1bf69a23c5
p4rseexp <p4rseexp@gmail.com>
2023-03-22 15:46:40 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ def initdb(drop):
     """Initialize the database."""
     if drop:
         db.drop_all()
+
     db.create_all()
     click.echo('Initialized database.')
 
@@ -64,8 +65,16 @@ def forge():
     db.session.commit()
     click.echo('Done.')
 
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+@app.errorhandler(404)
+def page_no_found(e):
+    return render_template('404.html'), 404 # 返回模板和状态码
+
 @app.route('/')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
